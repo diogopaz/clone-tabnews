e702f99c3c20a9155b6496f319c129156423e8a3
Diogo Paz <diogoachiles@gmail.com>
2024-06-14 18:12:02 -0300

    adding  and  scripts

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 function Home() {
-    return <>
-        <div>
-            <h1>Hello World</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, ex accusamus tempore repudiandae veritatis optio ipsa voluptatibus sunt modi nisi magni ut sequi nesciunt officiis libero architecto neque fuga maiores.
-
-            </p>
-        </div>
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, ex
+          accusamus tempore repudiandae veritatis optio ipsa voluptatibus sunt
+          modi nisi magni ut sequi nesciunt officiis libero architecto neque
+          fuga maiores.
+        </p>
+      </div>
     </>
+  );
 }
+
+function a = () {}
 
 export default Home;
