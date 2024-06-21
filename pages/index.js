79741c6797448b6d426e1acee00ecbb3a49e23e1
Diogo Paz <diogoachiles@gmail.com>
2024-06-21 22:00:17 +0000

    testing

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,14 @@ function Home() {
   return (
     <>
       <div>
-        <h1>Hello World</h1>
+        <h1>Clone Tabnews</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, ex
-          accusamus tempore repudiandae veritatis optio ipsa voluptatibus sunt
-          modi nisi magni ut sequi nesciunt officiis libero architecto neque
-          fuga maiores.
+          Este projeto é um clone do{" "}
+          <a href="https://www.tabnews.com.br/">TabNews</a> que estou realizando
+          no curso.dev
         </p>
       </div>
     </>
   );
 }
-
 export default Home;
