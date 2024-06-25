4b68d3a547dc722900b1e30d9b7ff6605151aa91
Diogo Paz <diogoachiles@gmail.com>
2024-06-25 09:20:18 -0300

    fix db credentials on `console.log()`

--- infra/database.js
+++ infra/database.js
@@ -10,13 +10,13 @@ async function query(queryObject) {
   });
   console.log(
     "Credenciais do Postgres: " +
-      {
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-      },
+      `{
+        host: ${process.env.POSTGRES_HOST},
+        port: ${process.env.POSTGRES_PORT},
+        user: ${process.env.POSTGRES_USER},
+        database: ${process.env.POSTGRES_DB},
+        password: ${process.env.POSTGRES_PASSWORD},
+      }`,
   );
 
   try {
